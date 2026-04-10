ab99ea4c0433a121c9c80ac352fcd06054811ab8
rohit-ningaraddi325 <151425469+rohit-ningaraddi325@users.noreply.github.com>
2026-04-10 12:29:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Database = require('better-sqlite3');
 const path = require('path');
-const logger = require('../utils/logger');
+const logger = require('./logger');
 
 const DB_PATH = process.env.DB_PATH || path.join(__dirname, '../../data/urls.db');
 
